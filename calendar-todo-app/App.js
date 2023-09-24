f9c04c6cde57215d379e769a91951bb572476b57
doyunj <doyun.jeong@the330.co.kr>
2023-09-23 23:39:36 -0400

    util / calendar initial code

--- calendar-todo-app/App.js
+++ calendar-todo-app/App.js
@@ -1,7 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { runPracticeDayjs } from "./src/practive-dayjs";
+import { getCalendarColumns } from "./src/util";
 
 export default function App() {
+  const now = dayjs();
+  const columns = getCalendarColumns(now);
+
+  useEffect(() => {
+    runPracticeDayjs();
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
